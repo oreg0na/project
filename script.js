76578e16b02090001c93929aee187088c56f42cf
Slava Pogosov <62066211+oreg0na@users.noreply.github.com>
2022-05-28 12:11:03 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+// переменные
+const title = 'title';
+const screens = 'screens';
+const screenPrice = 'screenPrice';
+const rollback = 'rollback';
+const fullPrice = 'fullPrice';
+const adaptive = 'adaptive';
+//
+alert('любое сообщение')
+console.log('любое сообщение')
