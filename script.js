92b75896d258ce58393cbadbd23b1ef3c1c7b6e8
Slava Pogosov <62066211+oreg0na@users.noreply.github.com>
2022-05-31 18:45:01 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,66 +1,33 @@
-'use strict';
-
-let title = "Авто из Японии";
-let screens = "Простые, Сложные, Интерактивные";
-let screenPrice = 3000;
-let rollback = 10;
-let fullPrice = 50000;
-let adaptive = true;
-
-console.log(typeof title);
-console.log(typeof fullPrice);
-console.log(typeof adaptive);
-
-console.log(screens.length);
-
-console.log("Стоимость верстки экранов " + screenPrice + " рублей.");
-console.log("Стоимость разработки сайта " + fullPrice + " рублей.");
-
-console.log(screens.toLowerCase().split(", "));
-console.log(fullPrice * rollback / 100);
+let title = prompt("Как называется ваш проект?");
+let screens = prompt("Какие типы экранов нужно разработать?");
+let screenPrice = parseInt(prompt("Сколько будет стоить данная работа?").replace(/[\s.,%]/g, ''))
+let rollback = 15;
+let adaptive = confirm("Нужен ли адаптив на сайте?");
+let service1 = prompt('Какой дополнительный тип услуги нужен?')
+let servicePrice1 = parseInt(prompt("Сколько это будет стоить?").replace(/[\s.,%]/g, ''))
+let service2 = prompt('Какой дополнительный тип услуги нужен?')
+let servicePrice2 = parseInt(prompt("Сколько это будет стоить?").replace(/[\s.,%]/g, ''))
 
 
+let fullPrice = screenPrice + servicePrice1 + servicePrice2
 
-const title = prompt("Как называется ваш проект?");
-console.log(title); 
 
-const screens = prompt('Какие типы экранов нужно разработать?', "Простые, Сложные, Интерактивные");
-console.log(screens);
+servicePercentPrice = Math.ceil(fullPrice - (fullPrice * (rollback / 100)))
+console.log(servicePercentPrice)
 
-const screenPrice = +prompt("Сколько будет стоить данная работа?");
-console.log(screenPrice);
-
-const adaptive = +prompt("Нужен ли адаптив на сайте?");
-// console.log(adaptive);
-
-let service1 = +prompt("Нужно ли перевести сайт на английский язык?");
-console.log(service1);
-
-let servicePrice1 = +prompt("Введите сколько будет стоить перевод на английский язык?");
-console.log(servicePrice1);
-
-let service2 = +prompt("Нужно ли перевести сайт на арабский язык?");
-console.log(service2);
-
-let servicePrice2 = +prompt("Введите сколько будет стоить перевод на арабскский язык?");
-console.log(servicePrice2);
-
-const fullPrice = screenPrice + servicePrice1 + servicePrice2;
-
-let servicePercentPrice = Math.ceil(fullPrice - fullPrice * rollback / 100);
-console.log(servicePercentPrice);
-
-if (fullPrice > 30000) {
-    console.log("Даем скидку в 10%");
-}
-if (fullPrice <= 30000 && fullPrice > 15000) {
-    console.log("Даем скидку в 5%");
-}
-if (fullPrice <= 15000 && fullPrice > 0) {
-    console.log("Скидка не предусмотрена");
-}
-if (fullPrice <= 0) {
-    console.log("Что то пошло не так");
-}
+switch (true) {
+    case (fullPrice >= 30000):
+        console.log("Даем скидку в 10 %")
+        break;
+    case (fullPrice >= 15000 && fullPrice < 30000):
+        console.log("Даем скидку в 5 %")
+        break;
+    case (fullPrice > 0 && fullPrice < 15000):
+        console.log("Скидка не предусмотрена")
+        break;
+    case (fullPrice <= 0):
+        console.log("Что то пошло не так")
+        break;
+    default:
         break;
 }
