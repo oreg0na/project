14aa33ad93ab6cf0079f112976cb83dc058e94d1
Slava Pogosov <62066211+oreg0na@users.noreply.github.com>
2022-06-04 20:50:06 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,97 +1,61 @@
 'use strict';
 
-let title;
-let screens;
-let screenPrice;
-let service1;
-let service2;
-let adaptive;
+console.log(isNaN(parseFloat('enterNumber')));
 
-const isNumber = function (num) {
-    return !isNaN(parseFloat(num)) && isFinite(num);
-};
+const hiddenNumber = function (myNumber) {
+    let startGame;
+    let gameOver;
 
-const asking = function () {
-    title = prompt('Как называется ваш проект?', 'Название проекта');
-    screens = prompt('Какие типы экранов нужно разработать?', 'Простые, Сложные');
+    startGame = confirm("Угадай число от 1 до 100?");
 
-    do {
-        screenPrice = prompt('Сколько будет стоить данная работа?');
-    }
-    while (!isNumber(screenPrice));
+    if (startGame) {
 
-    adaptive = confirm('Нужен ли адаптив на сайте?');
-};
+        takeNumber(myNumber);
 
-const showTypeOf = function (variable) {
-    console.log(variable, typeof variable);
-};
-
-const getRollbackMessage = function (price) {
-    if (price > 30000) {
-        return 'Даем скидку в 10%';
-    } else if (15000 < price && price <= 30000) {
-        return 'Даем скидку в 5%';
-    } else if (0 <= price && price <= 15000) {
-        return 'Скидка не предусмотрена';
     } else {
-        return 'Что то пошло не так';
+        gameOver = alert("Игра окончена");
     }
 };
 
-const getAllServicePrices = function () {
-    let sum = 0;
-    let num1,
-        num2;
+const takeNumber = function (unknowNum) {
+    let enterNumber;
 
-    for (let i = 0; i < 2; i++) {
+    enterNumber = prompt("Введите число от 1 до 100");
 
-        if (i === 0) {
-            service1 = prompt('Какой дополнительный тип услуги нужен?', 'Дополнительная услуга 1');
-
-            do {
-                num1 = prompt('Сколько это будет стоить?');
-            }
-            while (!isNumber(num1));
-
-        } else if (i === 1) {
-            service2 = prompt('Какой дополнительный тип услуги нужен?', 'Дополнительная услуга 2');
-
-            do {
-                num2 = prompt('Сколько это будет стоить?');
-            }
-            while (!isNumber(num2));
-        }
+    if (enterNumber === null) {
+        return alert("Игра окончена");
     }
-    sum = +num1.trim() + +num2.trim();
-    return sum;
+
+    while (enterNumber.trim() == '' || !isFinite(enterNumber)) {
+        alert("Введите число!");
+        enterNumber = prompt("Введите число от 1 до 100");
+    }
+
+    enterNumber = Number(enterNumber);
+
+    if (enterNumber < unknowNum) {
+        numLessAlert();
+    }
+
+    if (enterNumber > unknowNum) {
+        numMoreAlert();
+    }
+
+    if (enterNumber === unknowNum) {
+        return hiddenNumber == alert("Поздравляю, Вы угадали!!!");
+    }
+
+    takeNumber(unknowNum);
 };
 
-function getFullPrice(screenPrice, allServicePrices) {
-    return screenPrice + allServicePrices;
+const numLessAlert = function () {
+
+    alert("Загаданное число больше. Попробуйте еще...");
 };
 
-function getTitle(string) {
-    let lowCase = string.trim().toLowerCase();
-    let uppCase = lowCase.charAt(0).toUpperCase() + lowCase.slice(1);
-    return uppCase;
+const numMoreAlert = function () {
+
+    alert("Загаданное число меньше.Попробуйте еще...");
 };
 
-function getServicePercentPrices(fullPrice, rollback) {
-    return fullPrice - rollback;
-};
-
-asking();
-const allServicePrices = getAllServicePrices();
-const fullPrice = getFullPrice(+screenPrice.trim(), allServicePrices);
-const rollback = fullPrice * (15 / 100);
-const servicePercentPrice = Math.ceil(getServicePercentPrices(fullPrice, rollback));
-title = getTitle(title);
-
-showTypeOf(title);
-showTypeOf(fullPrice);
-showTypeOf(adaptive);
-
-console.log(screens.toLowerCase().split(", "));
-console.log(getRollbackMessage(fullPrice));
-console.log('Итоговая стоимость за вычетом отката посреднику', servicePercentPrice);
+hiddenNumber(55);
