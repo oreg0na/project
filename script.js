90b7892b0a4af9221fd7b5db85cd101d223e8202
Slava Pogosov <62066211+oreg0na@users.noreply.github.com>
2022-06-04 21:36:51 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,61 +1,40 @@
 'use strict';
 
-console.log(isNaN(parseFloat('enterNumber')));
-
-const hiddenNumber = function (myNumber) {
-    let startGame;
-    let gameOver;
-
-    startGame = confirm("Угадай число от 1 до 100?");
-
-    if (startGame) {
-
-        takeNumber(myNumber);
-
-    } else {
-        gameOver = alert("Игра окончена");
-    }
+const isNumber = function (num) {
+    return !isNaN(parseFloat(num)) && isFinite(num);
 };
 
-const takeNumber = function (unknowNum) {
-    let enterNumber;
+function gameBot(counter) {
+    let x = Math.ceil(Math.random() * 100);
+    function compare() {
+        const userNumber = prompt('Угадай число от 1 до 100');
 
-    enterNumber = prompt("Введите число от 1 до 100");
-
-    if (enterNumber === null) {
-        return alert("Игра окончена");
+        if (userNumber == null) {
+            alert('Спасибо за игру, до свидания :)');
+        } else if (counter === 0) {
+            if (true === confirm('Попытки закончились, хотите сыграть еще?')) {
+                gameBot(9);
+            } else { alert('Спасибо за игру, до свидания :)') }
+        } else if (!isNumber(userNumber)) {
+            counter--;
+            alert('Введи число! Осталось попыток: ' + (counter + 1));
+            compare();
+        } else if (userNumber < x) {
+            counter--;
+            alert('Загаданное число больше. Осталось попыток: ' + (counter + 1));
+            compare();
+        } else if (userNumber > x) {
+            counter--;
+            alert('Загаданное число меньше. Осталось попыток: ' + (counter + 1));
+            compare();
+        } else if (userNumber == x) {
+            if (true == confirm('Поздравляю, Вы угадали!!! Хотели бы сыграть еще?')) {
+                gameBot(9);
+            } else { alert('Спасибо за игру, до свидания :)') }
+        }
     }
 
-    while (enterNumber.trim() == '' || !isFinite(enterNumber)) {
-        alert("Введите число!");
-        enterNumber = prompt("Введите число от 1 до 100");
-    }
-
-    enterNumber = Number(enterNumber);
-
-    if (enterNumber < unknowNum) {
-        numLessAlert();
-    }
-
-    if (enterNumber > unknowNum) {
-        numMoreAlert();
-    }
-
-    if (enterNumber === unknowNum) {
-        return hiddenNumber == alert("Поздравляю, Вы угадали!!!");
-    }
-
-    takeNumber(unknowNum);
+    compare();
 };
 
-const numLessAlert = function () {
-
-    alert("Загаданное число больше. Попробуйте еще...");
-};
-
-const numMoreAlert = function () {
-
-    alert("Загаданное число меньше.Попробуйте еще...");
-};
-
-hiddenNumber(55);
+gameBot(9);
