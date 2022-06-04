d60f9a0410fb3a2947b463e65aeb00ff2faa4709
Slava Pogosov <62066211+oreg0na@users.noreply.github.com>
2022-06-04 21:15:50 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,57 +1,40 @@
-'use strict';
+' use strict';
 
-const title = getTitle(prompt('Как называется ваш проект?', 'Название проекта'));
-const screens = prompt('Какие типы экранов нужно разработать?', 'Простые, Сложные, Интерактивные');
-const screenPrice = parseFloat(prompt('Сколько будет стоить данная работа?', '10000'));
-const service1 = prompt('Какой дополнительный тип услуги нужен?', 'Дополнительная услуга 1');
-const servicePrice1 = parseFloat(prompt('Сколько это будет стоить?', '2000'));
-const service2 = prompt('Какой дополнительный тип услуги нужен?', 'Дополнительная услуга 2');
-const servicePrice2 = parseFloat(prompt('Сколько это будет стоить?', '3000'));
-const adaptive = confirm('Нужен ли адаптив на сайте?');
+let title = '"Project1" - обязательное поле* ';
 
-const showTypeOf = function (variable) {
-    console.log(variable, typeof variable);
+let getAllServicePrices = function (service1, service2) {
+    return service1 + service2;
 };
+let allServicePrices = getAllServicePrices(1000, 3000);
 
-const getRollbackMessage = function (price) {
-    if (price > 30000) {
-        return 'Даем скидку в 10%';
-    } else if (15000 < price && price <= 30000) {
-        return 'Даем скидку в 5%';
-    } else if (0 <= price && price >= 15000) {
-        return 'Скидка не предусмотрена';
-    } else {
-        return 'Что то пошло не так';
-    }
-};
-
-const getAllServicePrices = function (servicePrice1, servicePrice2) {
-    return servicePrice1 + servicePrice2;
-};
+let fullPrice;
 
 function getFullPrice(screenPrice, allServicePrices) {
     return screenPrice + allServicePrices;
-};
 
-function getTitle(string) {
-    let lowCase = string.trim().toLowerCase();
-    let uppCase = lowCase.charAt(0).toUpperCase() + lowCase.slice(1);
-    return uppCase;
+}
+let fullPrice = getFullPrice(1000, 2000);
+
+const getTitle = function (title) {
+    let firstCharBig;
+
+    do {
+        title = title.trim();
+    } while (title.indexOf(' ') === 0);
+
+    title = title.toLowerCase();
+    firstCharBig = title[0].toUpperCase();
+    title = title.replace(title[0], firstCharBig);
+    return title;
 };
 
 function getServicePercentPrices(fullPrice, rollback) {
     return fullPrice - rollback;
-};
+}
+let servicePercentPrice = getFullPrice(fullPrice / 100 * 10);
+console.log(servicePercentPrice);
 
-const allServicePrices = getAllServicePrices(servicePrice1, servicePrice2);
-const fullPrice = getFullPrice(screenPrice, allServicePrices);
-const rollback = fullPrice * (15 / 100);
-const servicePercentPrice = Math.ceil(getServicePercentPrices(fullPrice, rollback));
-
-showTypeOf(title);
-showTypeOf(fullPrice);
-showTypeOf(adaptive);
-
-console.log(screens.toLowerCase().split(", "));
-console.log(getRollbackMessage(fullPrice));
-console.log('Итоговая стоимость за вычетом отката посреднику', servicePercentPrice);
+console.log(allServicePrices);
+console.log(fullPrice);
+console.log(getTitle);
+console.log(servicePercentPrice);
